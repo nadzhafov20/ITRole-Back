374677542ebdaee932a73d9f5ee27c9e9b0bca1b
g1Den1s-it <g1den1splay@gmail.com>
2023-04-13 20:50:47 +0300

    added path in urls and added to apps

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Role(models.Model):
     name = models.CharField(verbose_name="name of IT proffesion",max_length=64, unique=True)
+    url_to_program = models.URLField(verbose_name="URL to bachelor program")
     description = models.TextField(default='emty', verbose_name="About this proffesion")
     video = models.URLField(verbose_name="link to some video about this proffesion")
 
@@ -14,6 +15,8 @@ class Role(models.Model):
 
 class Question(models.Model):
     question = models.TextField(verbose_name="write a question in here field")
+    positive_choice = models.TextField(verbose_name="positive choice", default="Yes")
+    negative_choice = models.TextField(verbose_name="negative choice", default="No")
     point = models.IntegerField(default=1)
     role = models.ForeignKey(Role, to_field="name",verbose_name="select where this question to be", on_delete=models.CASCADE)
 
--- it_role/urls.py
+++ it_role/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include("api.urls")),
+    path('', include('admin_panel.urls'))
 ]
